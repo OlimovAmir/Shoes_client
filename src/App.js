6236d04d6b3b8e5128d1b87ef9c0c629c8ef730a
olimov Amir <olimov.amir@mail.ru>
2024-02-21 19:16:08 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header/Header';
+import MenuSection from './components/header/menuSection/MenuSection';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Header/>
       </header>
+      <div>
+        <MenuSection/>
+      </div>
     </div>
   );
 }
